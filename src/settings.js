51e16b264c96bf58335a71c7d559162e22c3baf9
jotavebvc <jotavebvc@gmail.com>
2022-09-25 20:35:26 -0300

    PROXY DONE PARÇA+1

--- src/settings.js
+++ src/settings.js
@@ -1,1 +1,1 @@
-export const serverUrl = process.env.REACT_APP_SERVER_URL || 'https://vistapp-backend.herokuapp.com/api'
+export const serverUrl = process.env.REACT_APP_SERVER_URL || 'https://master.dcm6ft6t9fcis.amplifyapp.com/api'
